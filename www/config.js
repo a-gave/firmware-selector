855a794f7ecadfbe154b453a49da3499d138b034
a-gave <agave@dracaena.it>
2026-03-04 19:50:50 +0100

    add libremesh feeds

--- www/config.js
+++ www/config.js
@@ -5,21 +5,49 @@ var config = {
   show_help: true,
 
   // Versions list (optional if provided by .versions.json)
-  versions: ["23.05.4", "19.07.10"],
+  versions: [
+    "SNAPSHOT",
+    "25.12-SNAPSHOT",
+    "25.12.0",
+    "24.10-SNAPSHOT",
+    "24.10.5",
+    "23.05-SNAPSHOT",
+    "23.05.6",
+  ],
 
   // Pre-selected version (optional if provided by .versions.json)
-  default_version: "23.05.4",
+  // default_version: "24.10.5",
 
   // Image download URL (e.g. "https://downloads.openwrt.org")
-  image_url: "../misc",
+  image_url: "https://downloads.openwrt.org",
+  feed_url: "https://downloads.openwrt.org",
 
   // Insert snapshot versions (optional)
-  //show_snapshots: true,
+  show_snapshots: true,
+  upcoming_version: false,
 
   // Info link URL (optional)
   info_url: "https://openwrt.org/start?do=search&id=toh&q={title} @toh",
 
   // Attended Sysupgrade Server support (optional)
-  asu_url: "https://sysupgrade.openwrt.org",
-  asu_extra_packages: ["luci", "luci-app-attendedsysupgrade"],
+  // asu_url: "https://sysupgrade.openwrt.org",
+  // asu_extra_packages: ["luci", "luci-app-attendedsysupgrade"],
+  asu_url: "https://sysupgrade.antennine.org",
+  asu_fallback_url: "https://sysupgrade-01.antennine.org",
+  asu_extra_packages: [""],
+
+  lime: {
+    feed_url: "https://feed.libremesh.org",
+    pubkey_apk:
+      "-----BEGIN PUBLIC KEY-----\n\
+MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEdFJZ2qVti49Ol8LJZYuxgOCLowBS\n\
+8bI86a7zqhSbs5yon3JON7Yee7CQOgqwPOX5eMALGOu8iFGAqIRx5YjfYA==\n\
+-----END PUBLIC KEY-----",
+    pubkey_opkg: "RWSnGzyChavSiyQ+vLk3x7F0NqcLa4kKyXCdriThMhO78ldHgxGljM/8",
+    branches: [
+      // "main",
+      "master",
+      "dev",
+    ],
+  },
 };
